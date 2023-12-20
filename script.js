6a35f2159b4052bc9b61461dc82fa76a714204dc
belanond <belanondrej@gmail.com>
2023-12-20 20:40:05 +0100

    60 FPS

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ let doodlerWidth = 46;
 let doodlerHeight = 46;
 let doodlerX = boardWidth/2 - doodlerWidth/2;
 let doodlerY = boardHeight*7/8 - doodlerHeight;
-let doodlerRightImg;
-let doodlerLeftImg;
 
 let doodler = {
     img : null,
+    RightImg : null,
+    LeftImg : null,
     x : doodlerX,
     y : doodlerY,
     velocityLeft : 0,
@@ -24,7 +24,7 @@ let doodler = {
     velocityY : 0,
     jumpV : -8,
     width : doodlerWidth,
-    height : doodlerHeight
+    height : doodlerHeight,
 }
 
 //platforms
@@ -33,28 +33,47 @@ let platformWidth = 60;
 let platformHeight = 18;
 let platformImg;
 
+
+// GAME STATES
 let gameOver = false;
+let menu = false;
+let mobile = false;
+let hits = false;
+
+let now;
+let then;
+let elapsed;
+let fpsInterval;
+let fps = 60;
+
+function setGameOver(){
+    context.fillStyle = "black";
+    context.font = "16px sans-serif";
+    context.fillText("Game Over: Press 'Space' to Restart", boardWidth/7, boardHeight*12/13);
+}
+
+
 
 window.onload = function() {
 
     loadImages();
 
     board = document.getElementById("board");
-    console.log(board)
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d"); //used for drawing on the board
     placePlatforms();
 
 
-    doodler.img = doodlerRightImg;
+    doodler.img = doodler.RightImg;
     doodler.velocityY = doodler.jumpV;
-    doodlerRightImg.onload = function() {
+    doodler.RightImg.onload = function() {
         context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
     }
 
-
-    requestAnimationFrame(update);
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    requestAnimationFrame(frame);
     document.addEventListener("keydown", moveDoodler);
     document.addEventListener("keyup", (e) => {
         if (e.code === "ArrowLeft" || e.code === "KeyA")
@@ -64,14 +83,27 @@ window.onload = function() {
     });
 }
 
+function frame(){
+
+
+    requestAnimationFrame(frame);
+
+    now = Date.now();
+    elapsed = now - then;
+
+    if (elapsed > fpsInterval) {
+        then = now - (elapsed % fpsInterval);
+        requestAnimationFrame(update);
+    }
+}
+
 function update() {
-    requestAnimationFrame(update);
+
     if (gameOver) {
         return;
     }
+
     context.clearRect(0, 0, board.width, board.height);
-
-
 
     //platforms
     for (let i = 0; i < platformArray.length; i++) {
@@ -88,7 +120,7 @@ function update() {
     // clear platforms and add new platform
     while (platformArray.length > 0 && platformArray[0].y >= boardHeight) {
         platformArray.shift(); //removes first element from the array
-        newPlatform(); //replace with new platform on top
+        newPlatform(-platformHeight); //replace with new platform on top
     }
 
 
@@ -109,40 +141,23 @@ function update() {
     }
     context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
 
-
-
     if (gameOver) {
-        context.fillStyle = "black";
-        context.font = "16px sans-serif";
-        context.fillText("Game Over: Press 'Space' to Restart", boardWidth/7, boardHeight*12/13);
+        setGameOver();
     }
 }
 
 function moveDoodler(e) {
+
     if (e.code === "ArrowRight" || e.code === "KeyD") { //move right
         doodler.velocityRight = 4;
-        doodler.img = doodlerRightImg;
+        doodler.img = doodler.RightImg;
     }
     else if (e.code === "ArrowLeft" || e.code === "KeyA") { //move left
         doodler.velocityLeft = -4;
-        doodler.img = doodlerLeftImg;
+        doodler.img = doodler.LeftImg;
     }
     else if (e.code === "Space" && gameOver) {
-        //reset
-        doodler = {
-            img : doodlerRightImg,
-            x : doodlerX,
-            y : doodlerY,
-            velocityLeft : 0,
-            velocityRight : 0,
-            velocityY : doodler.jumpV,
-            jumpV : -8,
-            width : doodlerWidth,
-            height : doodlerHeight
-        }
-
-        gameOver = false;
-        placePlatforms();
+        reset()
     }
 }
 
@@ -160,25 +175,16 @@ function placePlatforms() {
     platformArray.push(platform);
 
     for (let i = 0; i < 6; i++) {
-        let randomX = Math.floor(Math.random() * boardWidth*3/4);
-        let platform = {
-            img : platformImg,
-            x : randomX,
-            y : boardHeight - 75*i - 150,
-            width : platformWidth,
-            height : platformHeight
-        }
-
-        platformArray.push(platform);
+        newPlatform(boardHeight - 75*i - 150);
     }
 }
 
-function newPlatform() {
+function newPlatform(height) {
     let randomX = Math.floor(Math.random() * boardWidth*3/4); //(0-1) * boardWidth*3/4
     let platform = {
         img : platformImg,
         x : randomX,
-        y : -platformHeight,
+        y : height,
         width : platformWidth,
         height : platformHeight
     }
@@ -186,21 +192,28 @@ function newPlatform() {
     platformArray.push(platform);
 }
 
+function reset(){
+    doodler.x = doodlerX;
+    doodler.y = doodlerY;
+
+    gameOver = false;
+    placePlatforms();
+}
 function detectCollision(doodler, platform) {
     return doodler.x < platform.x + platform.width &&   //a's top left corner doesn't reach b's top right corner
         doodler.x + doodler.width > platform.x &&   //a's top right corner passes b's top left corner
         doodler.y + doodler.height < platform.y + platform.height &&  //a's top left corner doesn't reach b's bottom left corner
         doodler.y + doodler.height > platform.y;    //a's bottom left corner passes b's top left corner
 }
-
 function loadImages() {
     // load images
-    doodlerRightImg = new Image();
-    doodlerRightImg.src = "./doodler-right.png";
+    doodler.RightImg = new Image();
+    doodler.RightImg.src = "./doodler-right.png";
 
-    doodlerLeftImg = new Image();
-    doodlerLeftImg.src = "./doodler-left.png";
+    doodler.LeftImg = new Image();
+    doodler.LeftImg.src = "./doodler-left.png";
 
     platformImg = new Image();
     platformImg.src = "./platform.png";
 }
+
